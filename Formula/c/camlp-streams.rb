3647a7c5db0a7a765c37344621eded135ccd17f0
Alexander Bayandin <alexander@bayandin.dev>
2025-01-19 21:02:10 +0000

    camlp-streams: revision bump for ocaml

--- Formula/c/camlp-streams.rb
+++ Formula/c/camlp-streams.rb
@@ -4,7 +4,7 @@ class CamlpStreams < Formula
   url "https://github.com/ocaml/camlp-streams/archive/refs/tags/v5.0.1.tar.gz"
   sha256 "ad71f62406e9bb4e7fb5d4593ede2af6c68f8b0d96f25574446e142c3eb0d9a4"
   license "LGPL-2.1-only" => { with: "OCaml-LGPL-linking-exception" }
-  revision 3
+  revision 4
 
   bottle do
     sha256 cellar: :any,                 arm64_sequoia: "9c01e19dbc10f3a4af763dc523057b9d9ed8a228bfe112f95750b9c8b3bfc2e4"
