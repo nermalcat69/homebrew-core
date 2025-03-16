7b2757664dcbf770d082d187ed67b99fc4a6b307
Carlo Cabrera <github@carlo.cab>
2025-03-16 16:52:03 +0800

    pypy3.11 7.3.17 (new formula)

---
+++ Formula/p/pypy3.11.rb
@@ -0,0 +1,240 @@
+class Pypy311 < Formula
+  desc "Implementation of Python 3 in Python"
+  homepage "https://pypy.org/"
+  url "https://downloads.python.org/pypy/pypy3.11-v7.3.19-src.tar.bz2"
+  sha256 "4817c044bb469a3274e60aa3645770f81eb4f9166ea7fdc4e6c351345554c8d8"
+  license "MIT"
+  head "https://github.com/pypy/pypy.git", branch: "main"
+
+  livecheck do
+    url "https://downloads.python.org/pypy/"
+    regex(/href=.*?pypy3(?:\.\d+)*[._-]v?(\d+(?:\.\d+)+)-src\.t/i)
+  end
+
+  depends_on "pkgconf" => :build
+  depends_on "pypy" => :build
+  depends_on "gdbm"
+  depends_on "openssl@3"
+  depends_on "sqlite"
+  depends_on "tcl-tk@8"
+  depends_on "xz"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "expat"
+  uses_from_macos "libffi"
+  uses_from_macos "ncurses"
+  uses_from_macos "unzip"
+  uses_from_macos "zlib"
+
+  def abi_version
+    stable.url[/pypy(\d+\.\d+)/, 1]
+  end
+
+  def newest_abi_version?
+    self == Formula["pypy3"]
+  end
+
+  link_overwrite "bin/pypy3", "lib/libpypy3-c"
+
+  # setuptools >= 60 required sysconfig patch
+  # See https://github.com/Homebrew/homebrew-core/pull/99892#issuecomment-1108492321
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/ef/75/2bc7bef4d668f9caa9c6ed3f3187989922765403198243040d08d2a52725/setuptools-59.8.0.tar.gz"
+    sha256 "09980778aa734c3037a47997f28d6db5ab18bdf2af0e49f719bfc53967fd2e82"
+  end
+
+  # always pull the latest pip, https://pypi.org/project/pip/#files
+  resource "pip" do
+    url "https://files.pythonhosted.org/packages/70/53/b309b4a497b09655cb7e07088966881a57d082f48ac3cb54ea729fd2c6cf/pip-25.0.1.tar.gz"
+    sha256 "88f96547ea48b940a3a385494e181e29fb8637898f88d88737c5049780f196ea"
+  end
+
+  # Build fixes:
+  # - Disable Linux tcl-tk detection since the build script only searches system paths.
+  #   When tcl-tk is not found, it uses unversioned `-ltcl -ltk`, which breaks build.
+  # Upstream issue ref: https://github.com/pypy/pypy/issues/3538
+  patch :DATA
+
+  def install
+    # The `tcl-tk` library paths are hardcoded and need to be modified for non-/usr/local prefix
+    tcltk = Formula["tcl-tk@8"]
+    inreplace "lib_pypy/_tkinter/tklib_build.py" do |s|
+      s.gsub! "['/usr/local/opt/tcl-tk/include']", "[]"
+      # We moved `tcl-tk` headers to `include/tcl-tk` and versioned TCL 8
+      # TODO: upstream this.
+      s.gsub! "(homebrew + '/include')", "('#{tcltk.opt_include}/tcl-tk')"
+      s.gsub! "(homebrew + '/opt/tcl-tk/lib')", "('#{tcltk.opt_lib}')"
+    end
+
+    if OS.mac?
+      # Allow python modules to use ctypes.find_library to find homebrew's stuff
+      # even if homebrew is not a /usr/local/lib. Try this with:
+      # `brew install enchant && pip install pyenchant`
+      inreplace "lib-python/3/ctypes/macholib/dyld.py" do |f|
+        f.gsub! "DEFAULT_LIBRARY_FALLBACK = [",
+                "DEFAULT_LIBRARY_FALLBACK = [ '#{HOMEBREW_PREFIX}/lib', "
+        f.gsub! "DEFAULT_FRAMEWORK_FALLBACK = [", "DEFAULT_FRAMEWORK_FALLBACK = [ '#{HOMEBREW_PREFIX}/Frameworks',"
+      end
+    end
+
+    # Having PYTHONPATH set can cause the build to fail if another
+    # Python is present, e.g. a Homebrew-provided Python 2.x
+    # See https://github.com/Homebrew/homebrew/issues/24364
+    ENV["PYTHONPATH"] = nil
+    ENV["PYPY_USESSION_DIR"] = buildpath
+
+    python = Formula["pypy"].opt_bin/"pypy"
+    cd "pypy/goal" do
+      system python, buildpath/"rpython/bin/rpython",
+             "-Ojit", "--shared", "--cc", ENV.cc, "--verbose",
+             "--make-jobs", ENV.make_jobs, "targetpypystandalone.py"
+
+      with_env(PYTHONPATH: buildpath) do
+        system "./pypy#{abi_version}-c", buildpath/"lib_pypy/pypy_tools/build_cffi_imports.py"
+      end
+    end
+
+    libexec.mkpath
+    cd "pypy/tool/release" do
+      package_args = %w[--archive-name pypy3 --targetdir . --no-make-portable --no-embedded-dependencies]
+      system python, "package.py", *package_args
+      system "tar", "-C", libexec.to_s, "--strip-components", "1", "-xf", "pypy3.tar.bz2"
+    end
+
+    # The PyPy binary install instructions suggest installing somewhere
+    # (like /opt) and symlinking in binaries as needed. Specifically,
+    # we want to avoid putting PyPy's Python.h somewhere that configure
+    # scripts will find it.
+    bin.install_symlink libexec/"bin/pypy#{abi_version}"
+    lib.install_symlink libexec/"bin"/shared_library("libpypy#{abi_version}-c")
+    include.install_symlink libexec/"include/pypy#{abi_version}"
+
+    if newest_abi_version?
+      bin.install_symlink "pypy#{abi_version}" => "pypy3"
+      lib.install_symlink shared_library("libpypy#{abi_version}-c") => shared_library("libpypy3-c")
+    end
+
+    return unless OS.linux?
+
+    # Delete two files shipped which we do not want to deliver
+    # These files make patchelf fail
+    rm([libexec/"bin/libpypy#{abi_version}-c.so.debug", libexec/"bin/pypy#{abi_version}.debug"])
+  end
+
+  def post_install
+    # Precompile cffi extensions in lib_pypy
+    # list from create_cffi_import_libraries in pypy/tool/release/package.py
+    %w[_sqlite3 _curses syslog gdbm _tkinter].each do |module_name|
+      quiet_system bin/"pypy#{abi_version}", "-c", "import #{module_name}"
+    end
+
+    # Post-install, fix up the site-packages and install-scripts folders
+    # so that user-installed Python software survives minor updates, such
+    # as going from 1.7.0 to 1.7.1.
+
+    # Create a site-packages in the prefix.
+    site_packages(HOMEBREW_PREFIX).mkpath
+    touch site_packages(HOMEBREW_PREFIX)/".keepme"
+    rm_r(site_packages(libexec))
+
+    # Symlink the prefix site-packages into the cellar.
+    site_packages(libexec).parent.install_symlink site_packages(HOMEBREW_PREFIX)
+
+    # Tell distutils-based installers where to put scripts
+    scripts_folder.mkpath
+    (distutils/"distutils.cfg").atomic_write <<~EOS
+      [install]
+      install-scripts=#{scripts_folder}
+    EOS
+
+    %w[setuptools pip].each do |pkg|
+      resource(pkg).stage do
+        system bin/"pypy#{abi_version}", "-s", "setup.py", "--no-user-cfg", "install", "--force", "--verbose"
+      end
+    end
+
+    # Symlinks to pip_pypy3
+    bin.install_symlink scripts_folder/"pip#{abi_version}" => "pip_pypy#{abi_version}"
+    symlink_to_prefix = [bin/"pip_pypy#{abi_version}"]
+
+    if newest_abi_version?
+      bin.install_symlink "pip_pypy#{abi_version}" => "pip_pypy3"
+      symlink_to_prefix << (bin/"pip_pypy3")
+    end
+
+    # post_install happens after linking
+    (HOMEBREW_PREFIX/"bin").install_symlink symlink_to_prefix
+  end
+
+  def caveats
+    <<~EOS
+      A "distutils.cfg" has been written to:
+        #{distutils}
+      specifying the install-scripts folder as:
+        #{scripts_folder}
+
+      If you install Python packages via "pypy#{abi_version} setup.py install" or pip_pypy#{abi_version},
+      any provided scripts will go into the install-scripts folder
+      above, so you may want to add it to your PATH *after* #{HOMEBREW_PREFIX}/bin
+      so you don't overwrite tools from CPython.
+
+      Setuptools and pip have been installed, so you can use pip_pypy#{abi_version}.
+      To update pip and setuptools between pypy#{abi_version} releases, run:
+          pip_pypy#{abi_version} install --upgrade pip setuptools
+
+      See: https://docs.brew.sh/Homebrew-and-Python
+    EOS
+  end
+
+  # The HOMEBREW_PREFIX location of site-packages
+  def site_packages(root)
+    root/"lib/pypy#{abi_version}/site-packages"
+  end
+
+  # Where setuptools will install executable scripts
+  def scripts_folder
+    HOMEBREW_PREFIX/"share/pypy#{abi_version}"
+  end
+
+  # The Cellar location of distutils
+  def distutils
+    site_packages(libexec).parent/"distutils"
+  end
+
+  test do
+    newest_pypy3_formula_name = CoreTap.instance
+                                       .formula_names
+                                       .select { |fn| fn.start_with?("pypy3") }
+                                       .max_by { |fn| Version.new(fn[/\d+\.\d+$/]) }
+
+    assert_equal Formula["pypy3"],
+                 Formula[newest_pypy3_formula_name],
+                 "The `pypy3` symlink needs to be updated!"
+    assert_equal abi_version, name[/\d+\.\d+$/]
+    system bin/"pypy#{abi_version}", "-c", "print('Hello, world!')"
+    system bin/"pypy#{abi_version}", "-c", "import time; time.clock()"
+    system scripts_folder/"pip#{abi_version}", "list"
+  end
+end
+
+__END__
+--- a/lib_pypy/_tkinter/tklib_build.py
++++ b/lib_pypy/_tkinter/tklib_build.py
+@@ -17,7 +17,7 @@ elif sys.platform == 'win32':
+     incdirs = []
+     linklibs = ['tcl86t', 'tk86t']
+     libdirs = []
+-elif sys.platform == 'darwin':
++else:
+     # homebrew
+     homebrew = os.environ.get('HOMEBREW_PREFIX', '')
+     incdirs = ['/usr/local/opt/tcl-tk/include']
+@@ -26,7 +26,7 @@ elif sys.platform == 'darwin':
+     if homebrew:
+         incdirs.append(homebrew + '/include')
+         libdirs.append(homebrew + '/opt/tcl-tk/lib')
+-else:
++if False: # disable Linux system tcl-tk detection
+     # On some Linux distributions, the tcl and tk libraries are
+     # stored in /usr/include, so we must check this case also
+     libdirs = []
