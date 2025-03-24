9b44b81661074934fe2a82932902b4ff46a24b5a
Michael Cho <michael@michaelcho.dev>
2025-03-23 22:04:13 -0400

    rocq 9.0.0 (renamed formula)

---
+++ Formula/r/rocq.rb
@@ -0,0 +1,103 @@
+class Rocq < Formula
+  desc "Proof assistant for higher-order logic"
+  homepage "https://rocq-prover.org/"
+  license "LGPL-2.1-only"
+
+  stable do
+    url "https://github.com/coq/coq/releases/download/V9.0.0/rocq-9.0.0.tar.gz"
+    sha256 "82f86646fd3d047f760837648195c73374beee667b1c9592d31c5426e3b43a51"
+
+    resource "stdlib" do
+      url "https://github.com/coq/stdlib/releases/download/V9.0.0/stdlib-9.0.0.tar.gz"
+      sha256 "1ab6adc42dfc651ddc909604bae1a54ff5623cda837f93677a8b12aab9eec711"
+    end
+  end
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  head do
+    url "https://github.com/coq/coq.git", branch: "master"
+
+    resource "stdlib" do
+      url "https://github.com/coq/stdlib.git", branch: "master"
+    end
+  end
+
+  depends_on "dune" => :build
+  depends_on "gmp"
+  depends_on "ocaml"
+  depends_on "ocaml-findlib"
+  depends_on "ocaml-zarith"
+
+  uses_from_macos "m4" => :build
+  uses_from_macos "unzip" => :build
+
+  def install
+    # Work around for https://github.com/Homebrew/homebrew-test-bot/issues/805
+    if ENV["HOMEBREW_GITHUB_ACTIONS"] && !(Formula["ocaml-findlib"].etc/"findlib.conf").exist?
+      ENV["OCAMLFIND_CONF"] = Formula["ocaml-findlib"].opt_libexec/"findlib.conf"
+    end
+    ENV.prepend_path "OCAMLPATH", Formula["ocaml-zarith"].opt_lib/"ocaml"
+    ENV.prepend_path "OCAMLPATH", Formula["ocaml-findlib"].opt_lib/"ocaml"
+
+    packages = %w[rocq-runtime coq-core rocq-core coq coqide-server]
+
+    system "./configure", "-prefix", prefix,
+                          "-mandir", man,
+                          "-libdir", HOMEBREW_PREFIX/"lib/ocaml/coq",
+                          "-docdir", pkgshare/"latex"
+    system "make", "dunestrap"
+    system "dune", "build", "-p", packages.join(",")
+    system "dune", "install", "--prefix=#{prefix}",
+                              "--mandir=#{man}",
+                              "--libdir=#{lib}/ocaml",
+                              "--docdir=#{doc.parent}",
+                              *packages
+
+    resource("stdlib").stage do
+      ENV.prepend_path "PATH", bin
+      ENV["ROCQLIB"] = lib/"ocaml/coq"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    # Work around for https://github.com/Homebrew/homebrew-test-bot/issues/805
+    if ENV["HOMEBREW_GITHUB_ACTIONS"] && !(Formula["ocaml-findlib"].etc/"findlib.conf").exist?
+      ENV["OCAMLFIND_CONF"] = Formula["ocaml-findlib"].opt_libexec/"findlib.conf"
+    end
+    (testpath/"testing.v").write <<~EOS
+      Require Stdlib.micromega.Lia.
+      Require Stdlib.ZArith.ZArith.
+
+      Inductive nat : Set :=
+      | O : nat
+      | S : nat -> nat.
+      Fixpoint add (n m: nat) : nat :=
+        match n with
+        | O => m
+        | S n' => S (add n' m)
+        end.
+      Lemma add_O_r : forall (n: nat), add n O = n.
+      Proof.
+      intros n; induction n; simpl; auto; rewrite IHn; auto.
+      Qed.
+
+      Import Stdlib.micromega.Lia.
+      Import Stdlib.ZArith.ZArith.
+      Open Scope Z.
+      Lemma add_O_r_Z : forall (n: Z), n + 0 = n.
+      Proof.
+      intros; lia.
+      Qed.
+    EOS
+    system bin/"rocq", "compile", testpath/"testing.v"
+    # test ability to find plugin files
+    output = shell_output("#{Formula["ocaml-findlib"].bin}/ocamlfind query rocq-runtime.plugins.ltac")
+    assert_equal "#{HOMEBREW_PREFIX}/lib/ocaml/rocq-runtime/plugins/ltac", output.chomp
+  end
+end
