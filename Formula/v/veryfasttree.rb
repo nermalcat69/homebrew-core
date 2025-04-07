794d1428aa84697b1ad9374fae48ecc0b1fbe10c
Rui Chen <rui@chenrui.dev>
2025-04-07 22:30:59 +0000

    veryfasttree 4.0.5
    autobump: add veryfasttree
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/veryfasttree.rb
+++ Formula/v/veryfasttree.rb
@@ -1,8 +1,8 @@
 class Veryfasttree < Formula
   desc "Efficient phylogenetic tree inference for massive taxonomic datasets"
   homepage "https://github.com/citiususc/veryfasttree"
-  url "https://github.com/citiususc/veryfasttree/archive/refs/tags/v4.0.4.tar.gz"
-  sha256 "27c779164f4fa0c75897a6e95b35f820a2a10e7c244b8923c575e0ea46f15f6b"
+  url "https://github.com/citiususc/veryfasttree/archive/refs/tags/v4.0.5.tar.gz"
+  sha256 "e753c01555b3363747ea1d51248d691aa1e79d228cac187a6725ea8cd86ad321"
   license all_of: [
     "GPL-3.0-only",
     "BSD-3-Clause", # libs/cli11
