c893e85d1cc6249ecd8faa930f47fe461b1ab491
bevanjkay <email@bevankay.me>
2025-04-09 23:51:06 +1000

    fbthrift 2025.04.07.00

--- Formula/f/fbthrift.rb
+++ Formula/f/fbthrift.rb
@@ -1,8 +1,8 @@
 class Fbthrift < Formula
   desc "Facebook's branch of Apache Thrift, including a new C++ server"
   homepage "https://github.com/facebook/fbthrift"
-  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2025.03.10.00.tar.gz"
-  sha256 "68f827e59de74587b4deed7cad52452f79b5bf2e89c04312011cbb4b34594b6a"
+  url "https://github.com/facebook/fbthrift/archive/refs/tags/v2025.04.07.00.tar.gz"
+  sha256 "c186db1b797be72d5d84265a15378012329427ab5324b5af127d1f641b2ba700"
   license "Apache-2.0"
   head "https://github.com/facebook/fbthrift.git", branch: "main"
 
