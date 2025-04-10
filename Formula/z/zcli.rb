f74bf3327e2d2bb06f43f9b6bae31f93aa8dc15d
Arjun Aditya <meow@nermalcat69.dev>
2025-04-10 18:34:11 +0530

    Update zcli.rb

--- Formula/z/zcli.rb
+++ Formula/z/zcli.rb
@@ -5,13 +5,13 @@ class Zcli < Formula
     license "MIT"
   
     if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/zeropsio/zcli/releases/download/v#{version}/zcli-darwin-arm64-npm.tar.gz"
+      url "https://github.com/zeropsio/zcli/releases/download/v1.0.41/zcli-darwin-arm64-npm.tar.gz"
       sha256 "4e7974eb98b3ec34c93f06c61126ae02c4cc249a11a17d9d1e468e9fc1a42ec0"
     elsif OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/zeropsio/zcli/releases/download/v#{version}/zcli-darwin-amd64-npm.tar.gz"
+      url "https://github.com/zeropsio/zcli/releases/download/v1.0.41/zcli-darwin-amd64-npm.tar.gz"
       sha256 "07c25ea766415438918e25351065ad2b8d63801230ecb7f1eb21c5aaeaa3735b"
     elsif OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/zeropsio/zcli/releases/download/v#{version}/zcli-linux-amd64-npm.tar.gz"
+      url "https://github.com/zeropsio/zcli/releases/download/v1.0.41/zcli-linux-amd64-npm.tar.gz"
       sha256 "87792136a52c11557d1973c815ffde184d17ba110407e07700a317cc1ceaec1c"
     else
       odie "Unsupported platform for zcli"
