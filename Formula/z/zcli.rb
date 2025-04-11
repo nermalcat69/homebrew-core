5a665e7e5330b8d7c522dc3d93d25f57d599961e
Arjun Aditya <meow@nermalcat69.dev>
2025-04-11 17:05:45 +0530

    Update zcli.rb

--- Formula/z/zcli.rb
+++ Formula/z/zcli.rb
@@ -1,34 +1,17 @@
 class Zcli < Formula
     desc "Zerops command line utility"
     homepage "https://github.com/zeropsio/zcli"
-    version "1.0.41"
-    license "MIT"
+    url "https://github.com/zeropsio/zcli/releases/download/v1.0.42/zcli_1.0.42_darwin_arm64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   
-    if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/zeropsio/zcli/releases/download/v1.0.41/zcli-darwin-arm64-npm.tar.gz"
-      sha256 "4e7974eb98b3ec34c93f06c61126ae02c4cc249a11a17d9d1e468e9fc1a42ec0"
-    elsif OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/zeropsio/zcli/releases/download/v1.0.41/zcli-darwin-amd64-npm.tar.gz"
-      sha256 "07c25ea766415438918e25351065ad2b8d63801230ecb7f1eb21c5aaeaa3735b"
-    elsif OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/zeropsio/zcli/releases/download/v1.0.41/zcli-linux-amd64-npm.tar.gz"
-      sha256 "87792136a52c11557d1973c815ffde184d17ba110407e07700a317cc1ceaec1c"
-    else
-      odie "Unsupported platform for zcli"
+    bottle do
+      sha256 cellar: :any_skip_relocation, arm64_sonoma: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 cellar: :any_skip_relocation, x86_64_linux: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   
-    livecheck do
-      url :stable
-      strategy :github_latest
-    end
-  
+    depends_on "wireguard-tools"
+
     def install
       bin.install "zcli"
     end
-  
-    test do
-      assert_match version.to_s, shell_output("#{bin}/zcli --version")
-      system "#{bin}/zcli", "help"
-    end
   end
-  
